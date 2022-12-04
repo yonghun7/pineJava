551c969a336e54545b7164a222558d9ccd6bb35c
yonghun7 <yonghun.choi@gmail.com>
2022-12-04 21:38:29 +0900

    12

--- MyWeb/src/main/java/kr/co/pinetree/hello/HelloMain.java
+++ MyWeb/src/main/java/kr/co/pinetree/hello/HelloMain.java
@@ -18,6 +18,6 @@ public class HelloMain {
 		
 		HelloService service = ctx.getBean("helloService", HelloService.class);
 		result = service.calcTwoNumbers(3, 5);
-		System.out.println("result service = "+result); 
+		System.out.println("result service = "+result);   
 	}
 }
